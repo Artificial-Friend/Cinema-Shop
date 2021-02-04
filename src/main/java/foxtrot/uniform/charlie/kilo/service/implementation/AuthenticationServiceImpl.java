751593b8c9412e3344b753d2e8d18691aa062b9e
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-02-04 13:20:32 +0200

    p4c8 AuthenticationServiceImpl refactored

--- src/main/java/foxtrot/uniform/charlie/kilo/service/implementation/AuthenticationServiceImpl.java
+++ src/main/java/foxtrot/uniform/charlie/kilo/service/implementation/AuthenticationServiceImpl.java
@@ -1,26 +1,26 @@
 package foxtrot.uniform.charlie.kilo.service.implementation;
 
-import foxtrot.uniform.charlie.kilo.dao.ShoppingCartDao;
-import foxtrot.uniform.charlie.kilo.dao.UserDao;
 import foxtrot.uniform.charlie.kilo.exception.AuthenticationException;
 import foxtrot.uniform.charlie.kilo.lib.Inject;
 import foxtrot.uniform.charlie.kilo.lib.Service;
-import foxtrot.uniform.charlie.kilo.model.ShoppingCart;
 import foxtrot.uniform.charlie.kilo.model.User;
 import foxtrot.uniform.charlie.kilo.service.AuthenticationService;
+import foxtrot.uniform.charlie.kilo.service.ShoppingCartService;
+import foxtrot.uniform.charlie.kilo.service.UserService;
 import foxtrot.uniform.charlie.kilo.util.HashUtil;
 import java.util.Optional;
 
+@SuppressWarnings("unused")
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
-    private UserDao userDao;
+    private UserService userService;
     @Inject
-    private ShoppingCartDao shoppingCartDao;
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        Optional<User> userByLogin = userDao.getByLogin(email);
+        Optional<User> userByLogin = userService.findByEmail(email);
         if (userByLogin.isPresent() && userByLogin.get().getPassword().equals(HashUtil.hashPassword(
                 password, userByLogin.get().getSalt()))) {
             return userByLogin.get();
@@ -30,16 +30,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public User register(String email, String password) {
-        Optional<User> userByEmail = userDao.getByLogin(email);
+        Optional<User> userByEmail = userService.findByEmail(email);
         if (userByEmail.isEmpty()) {
             User user = new User();
             user.setEmail(email);
             user.setSalt(HashUtil.getSalt());
             user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
-            ShoppingCart shoppingCart = new ShoppingCart();
-            shoppingCart.setUser(user);
-            shoppingCartDao.add(shoppingCart);
-            return userDao.add(user);
+            userService.add(user);
+            shoppingCartService.registerNewShoppingCart(user);
+            return user;
         }
         throw new AuthenticationException("ERROR: Unable to register user with email " + email);
     }
