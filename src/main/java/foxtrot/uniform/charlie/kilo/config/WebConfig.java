e4986a82ff18a835641c3b6232434658b7ef1d64
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-02-18 00:11:52 +0200

    p9c2 User.class redundant implementation removed

--- src/main/java/foxtrot/uniform/charlie/kilo/config/WebConfig.java
+++ src/main/java/foxtrot/uniform/charlie/kilo/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"foxtrot.uniform.charlie.kilo.controller",
-        "foxtrot.uniform.charlie.kilo.exception"})
+@ComponentScan(basePackages = "foxtrot.uniform.charlie.kilo.controller")
 public class WebConfig {
 }
--- src/main/java/foxtrot/uniform/charlie/kilo/model/User.java
+++ src/main/java/foxtrot/uniform/charlie/kilo/model/User.java
@@ -1,18 +1,15 @@
 package foxtrot.uniform.charlie.kilo.model;
 
-import java.util.Collection;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -42,36 +39,6 @@ public class User implements UserDetails {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
     }
 
     @Override
