22b1af7a88cc035bb52c36ebce15903a0fb2fa2b
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-02-18 17:17:07 +0200

    p9c5 fixed according to comments

--- src/main/java/foxtrot/uniform/charlie/kilo/config/SecurityConfig.java
+++ src/main/java/foxtrot/uniform/charlie/kilo/config/SecurityConfig.java
@@ -21,8 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/register")
-                .permitAll()
+                .antMatchers("/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
